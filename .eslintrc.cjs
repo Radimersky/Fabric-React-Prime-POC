3cc8f2456de21a687495ffec7aefded18037c80d
Marek Radimersky <marek.radimersky@chyron.com>
2024-07-23 13:24:41 +0200

    Fix linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,13 +8,22 @@ module.exports = {
     'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.app.json'
+  },
   plugins: ['react-refresh'],
   rules: {
+    'react/prop-types': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
