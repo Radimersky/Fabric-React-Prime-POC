672f5eccd9d6cfedd90859238c752228a405f0e9
Marek Radimersky <marek.radimersky@chyron.com>
2024-07-23 09:07:00 +0200

    Edit main

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Canvas from './Canvas.tsx'
+import { FabricContextProvider } from './FabricContextProvider.tsx'
+import CanvasOperation from './CanvasOperation.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <h2>Fabric.js canvas using Prime template POC</h2>
-    <Canvas />
+    <FabricContextProvider>
+      <>
+        <Canvas />
+        <CanvasOperation />
+      </>
+    </FabricContextProvider>
   </React.StrictMode>,
 )
